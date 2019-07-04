22fffeccd1a1dfa7aae90438f423388741954a7a
javatr6 <javafullstack.19sep@gmail.com>
2019-07-04 17:13:31 +0530

    Sales Class show method updated

--- MyFirstProject/src/main/java/org/cap/demo/SalesClass.java
+++ MyFirstProject/src/main/java/org/cap/demo/SalesClass.java
@@ -4,10 +4,13 @@ public class SalesClass {
 	
 	private int[] arr= {1,34,56,7};
 	
-	public void show() {
+	public int show() {
+		int sum=0;
 		for(int num:arr) {
+			sum+=num;
 			System.out.println(num);
 		}
+		return sum;
 	}
 
 }
