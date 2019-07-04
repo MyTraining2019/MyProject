cf4161b3c40652ec7f6216dbb9a1110ea65e827c
javatr6 <javafullstack.19sep@gmail.com>
2019-07-04 12:22:53 +0530

    Done changes in Main Method!

--- MyFirstProject/src/main/java/org/cap/demo/MainClass.java
+++ MyFirstProject/src/main/java/org/cap/demo/MainClass.java
@@ -13,6 +13,8 @@ public class MainClass {
 	
 	public static void display() {
 		System.out.println("Hello! Good Morning!");
+		System.out.println("Hello! I am working in Java");
+		System.out.println("I am Java Full Stack Developer!");
 	}
 
 }
