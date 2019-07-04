6a1cb305fb8f0a7c9cf74fc1d0d964dd1f700773
javatr6 <javafullstack.19sep@gmail.com>
2019-07-04 11:18:00 +0530

    my first commit with git.

---
+++ MyFirstProject/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,18 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome! Sales Application!");
+		SalesClass obj=new SalesClass();
+		obj.show();
+		display();
+
+	}
+	
+	public static void display() {
+		System.out.println("Hello! Good Morning!");
+	}
+
+}
