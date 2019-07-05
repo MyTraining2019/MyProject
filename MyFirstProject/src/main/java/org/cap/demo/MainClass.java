0f9ee41419454c73802009d332f09f1cfd834c11
javatr6 <javafullstack.19sep@gmail.com>
2019-07-05 10:11:51 +0530

    Main Method Updated

--- MyFirstProject/src/main/java/org/cap/demo/MainClass.java
+++ MyFirstProject/src/main/java/org/cap/demo/MainClass.java
@@ -3,7 +3,13 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void main(String[] args) {
-	
+		int oddsum=0;
+		for(int i=0;i<100;i++)
+		{
+			if(i%2!=0)
+				oddsum+=i;
+		}
+		System.out.println("Sum of Odd nums:" + oddsum);
 		System.out.println("Welcome! Sales Application!");
 		SalesClass obj=new SalesClass();
 		obj.show();
